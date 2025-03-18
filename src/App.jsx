7cabe234e6ab3d335deeeb0ad177d2fd5e9efa39
Muhammad <muhammad124711@gmail.com>
2025-03-18 11:36:21 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
